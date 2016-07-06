a8d00cc5c20fb2816f1781a6f65b717582f0688c
Eli Plaut <eli@sentinelone.com>
2016-07-06 14:48:10 +0300

    nrpe command escape arg2

--- add_nrpe_command.sh
+++ add_nrpe_command.sh
@@ -4,6 +4,6 @@ cat >>/usr/local/nagios/etc/objects/commands.cfg <<EOF
 
 define command{
         command_name check_nrpe
-        command_line \$USER1\$/check_nrpe -H \$HOSTADDRESS\$ -c \$ARG1\$ -a \$ARG2\$
+        command_line \$USER1\$/check_nrpe -H \$HOSTADDRESS\$ -c \$ARG1\$ -a "\$ARG2\$"
         }
 EOF
