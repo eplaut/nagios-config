e81b5f2a03e6718af5478a0f457f4929a8a18f11
Eli Plaut <eli@sentinelone.com>
2016-07-10 14:21:08 +0300

    check_nrpe to python

--- add_nrpe_command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-cat >>/usr/local/nagios/etc/objects/commands.cfg <<EOF
-
-define command{
-        command_name check_nrpe
-        command_line \$USER1\$/check_nrpe -H \$HOSTADDRESS\$ -c \$ARG1\$ -a "\$ARG2\$"
-        }
-EOF
